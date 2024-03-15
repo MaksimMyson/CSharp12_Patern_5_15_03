e86d245283e7e8ed303bae6557f749f7bd111791
MaksimMyson <maksimmyson33@gmail.com>
2024-03-15 20:38:16 +0200

    observer was done

--- CSharp12_Patern_5_15_03/Program.cs
+++ CSharp12_Patern_5_15_03/Program.cs
@@ -1,10 +1,83 @@
 ﻿namespace CSharp12_Patern_5_15_03
 {
-    internal class Program
+    interface IObserver
+    {
+        void Update(int temperature, int humidity);
+    }
+
+    // Конкретний спостерігач (наприклад, дисплей погоди)
+    class WeatherDisplay : IObserver
+    {
+        public void Update(int temperature, int humidity)
+        {
+            Console.WriteLine($"Weather display: Temperature {temperature}°C, Humidity {humidity}%");
+        }
+    }
+
+    // Інтерфейс суб'єкта
+    interface ISubject
+    {
+        void RegisterObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void NotifyObservers();
+    }
+
+    // Стан погоди 
+    class WeatherStation : ISubject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+        private int temperature;
+        private int humidity;
+
+        public void SetWeather(int temperature, int humidity)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            NotifyObservers();
+        }
+
+        public void RegisterObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(temperature, humidity);
+            }
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Створюємо стан погоди
+            WeatherStation weatherStation = new WeatherStation();
+
+            // Створюємо дисплеї погоди)
+            WeatherDisplay display1 = new WeatherDisplay();
+            WeatherDisplay display2 = new WeatherDisplay();
+
+            // Реєструємо дисплеї погоди у стан погоди
+            weatherStation.RegisterObserver(display1);
+            weatherStation.RegisterObserver(display2);
+
+            // Встановлюємо новий стан погоди
+            weatherStation.SetWeather(25, 70);
+
+            // Видаляємо одного зі дисплеїв погоди
+            weatherStation.RemoveObserver(display1);
+
+            // Встановлюємо новий стан погоди
+            weatherStation.SetWeather(20, 60);
         }
     }
 }
